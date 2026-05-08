79d1c0584116742e34175becb98c1cfdcfb89751
@tigrao <romastefale@gmail.com>
2026-05-07 22:29:25 -0300

    test: update parser expectations for enabled marketplaces

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -34,16 +34,35 @@ def test_clean_item_id_variants() -> None:
     assert clean_item_id("MLB1234567890") == "MLB1234567890"
 
 
-def test_non_mercado_livre_url_stays_unknown() -> None:
+def test_parse_shopee_url() -> None:
     product = parse_offer_input("https://shopee.com.br/produto-i.123456.987654321")
-    assert product.store == Store.UNKNOWN
-    assert product.product_id is None
+    assert product.store == Store.SHOPEE
+    assert product.product_id == "123456.987654321"
 
 
-def test_non_mercado_livre_id_stays_unknown() -> None:
-    product = parse_offer_input("B0ABCDEFGH")
-    assert product.store == Store.UNKNOWN
-    assert product.product_id is None
+def test_parse_shopee_short_link() -> None:
+    product = parse_offer_input("https://br.shp.ee/16iSJgnN")
+    assert product.store == Store.SHOPEE
+    assert product.url == "https://br.shp.ee/16iSJgnN"
+
+
+def test_parse_amazon_asin_and_short_links() -> None:
+    assert parse_offer_input("B0ABCDEFGH").store == Store.AMAZON
+    assert parse_offer_input("https://a.co/d/abc123").store == Store.AMAZON
+    assert parse_offer_input("https://amzn.to/abc123").store == Store.AMAZON
+
+
+def test_parse_aliexpress_links() -> None:
+    product = parse_offer_input("https://www.aliexpress.com/item/1005001234567890.html")
+    assert product.store == Store.ALIEXPRESS
+    assert product.product_id == "1005001234567890"
+    assert parse_offer_input("https://s.click.aliexpress.com/e/_ABAjz1").store == Store.ALIEXPRESS
+
+
+def test_parse_magalu_links() -> None:
+    assert parse_offer_input("https://www.magazineluiza.com.br/produto/p/abc123/").store == Store.MAGALU
+    assert parse_offer_input("https://maga.lu/abc123").store == Store.MAGALU
+    assert parse_offer_input("https://magalu.page.link/abc123").store == Store.MAGALU
 
 
 def test_parse_search() -> None:
